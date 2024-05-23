ca629b6cee9cdeb43cc3b7a87db8103b2c1d261f
ryanxnguy <ryannguyen137@gmail.com>
2024-05-23 16:54:00 -0700

    Added Item.cpp

---
+++ src/Item.cpp
@@ -0,0 +1,34 @@
+#include "Item.h"
+#include <iostream>
+
+using namespace std;
+
+Item::Item(const string& name) : name(name) {}
+
+string Item::getName() const {
+    return name;
+}
+
+
+Weapon::Weapon(Type type, int damage) 
+    : Item("Weapon"), type(type), damage(damage) {}
+
+Weapon::Type Weapon::getType() const {
+    return type;
+}
+
+int Weapon::getDamage() const {
+    return damage;
+}
+
+
+Potion::Potion(Type type, int strength) 
+    : Item("Potion"), type(type), strength(strength) {}
+
+Potion::Type Potion::getType() const {
+    return type;
+}
+
+int Potion::getStrength() const {
+    return strength;
+}
