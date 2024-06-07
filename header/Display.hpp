6340b007da895b2dd0d92e56afd97921b6c579ab
ryanxnguy <ryannguyen137@gmail.com>
2024-06-07 10:00:32 -0700

    Added display h file

---
+++ header/Display.hpp
@@ -0,0 +1,20 @@
+#ifndef __DISPLAY_HPP__
+#define __DISPLAY_HPP__
+
+#include <iostream>
+#include <string>
+
+class Display {
+	public:
+        static void printIntro();
+        static void printInvalidInput();
+        static void printCharacterCreation();
+        static void printSettings();
+        static void printQuit();
+        static void printStats();
+
+        //Print function headers for Item classes 
+};
+
+
+#endif
