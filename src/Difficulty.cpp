0acb205c99fd166d144336be194cbee0d3e036b2
Hugzaregood <93222903+Hugzaregood@users.noreply.github.com>
2024-06-06 21:05:19 -0700

    Updated misspelling for Difficulty.cpp

--- src/Difficulty.cpp
+++ src/Difficulty.cpp
@@ -8,21 +8,21 @@ using namespace std;
 #include "../header/Monster.hpp"
 #include "../header/Difficulty.hpp"
 
-pair<double, double> Diffculty::setEasy() {
+pair<double, double> Difficulty::setEasy() {
     Monster monster("EasyMonster", 100, 5, 100.0); // Example initialization
     double modifiedHealth = monster.getHealth() * 0.5;
     double modifiedAttackStrength = monster.getAttackStrength() * 0.5;
     return {modifiedHealth, modifiedAttackStrength};
 }
 
-pair<double, double> Diffculty::setNormal() {
+pair<double, double> Difficulty::setNormal() {
     Monster monster("NormalMonster", 100, 5, 100.0); // Example initialization
     double modifiedHealth = monster.getHealth();
     double modifiedAttackStrength = monster.getAttackStrength();
     return {modifiedHealth, modifiedAttackStrength};
 }
 
-pair<double, double> Diffculty::setHard() {
+pair<double, double> Difficulty::setHard() {
     Monster monster("HardMonster", 100, 5, 100.0); // Example initialization
     double modifiedHealth = monster.getHealth() * 1.5;
     double modifiedAttackStrength = monster.getAttackStrength() * 1.5;
