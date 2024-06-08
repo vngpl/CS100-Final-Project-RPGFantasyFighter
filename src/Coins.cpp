10e515dc5d397af661142ba4cbef381db535b8c2
Hugzaregood <nicholas.castellanos@yahoo.com>
2024-06-08 14:26:18 -0700

    Created a setter function in Coins class

--- src/Coins.cpp
+++ src/Coins.cpp
@@ -26,6 +26,7 @@ void Coins::addCoinsOnMONSERdeath() {
 
 int Coins::getAmountCoins() const { return amountCoins; }
 
+int Coins::setUpdateCoins(int newCoins) { newCoins = amountCoins; }
 
 // int coinsDropped = minCoins + std::rand() % (maxCoins - minCoins + 1);
 // amountCoins += coinsDropped;
