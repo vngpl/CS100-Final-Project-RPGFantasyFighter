c7063b38581f1948355af86495102a77388518da
vngpl <v.ngpl1911@gmail.com>
2024-06-11 15:14:40 -0700

    character constructor fixed

--- test/testCharacter.cpp
+++ test/testCharacter.cpp
@@ -2,17 +2,17 @@
 #include "../header/Character.hpp"
 
 TEST(CharacterTest, HealthTest) {
-    Character testCharacter(50, 100, "TEST CHARACTER");
+    Character testCharacter(50, 100, 5, "TEST CHARACTER");
     EXPECT_EQ(testCharacter.getHealth(), 50);
 }
 
 TEST(CharacterTest, LevelTest) {
-    Character testCharacter(100, 100, "TEST CHARACTER");
+    Character testCharacter(100, 100, 5, "TEST CHARACTER");
     EXPECT_EQ(testCharacter.getLevel(), 5);
 }
 
 TEST(CharacterTest, AddItemTest) {
-    Character testCharacter(100, 100, "TEST CHARACTER");
+    Character testCharacter(100, 100, 1, "TEST CHARACTER");
     testCharacter.addItem(new Item("SWORD", 20));
 
     const std::vector<Item*>& inventory = testCharacter.getInventoryItems();
@@ -22,13 +22,13 @@ TEST(CharacterTest, AddItemTest) {
 }
 
 TEST(CharacterTest, UpdateMonsterSlainCountTest) {
-    Character testCharacter(100, 100, "TEST CHARACTER");
+    Character testCharacter(100, 100, 5, "TEST CHARACTER");
     character.updateMonsterSlainCount();
     EXPECT_EQ(character.getMonstersSlain(), 1);
 }
 
 TEST(CharacterTest, UseItemTest) {
-    Character testCharacter(100, 100, "TEST CHARACTER");
+    Character testCharacter(100, 100, 5, "TEST CHARACTER");
     Item* item = new Item("SWORD", 20);
     testCharacter.addItem(item);
     testCharacter.useItem(0);
@@ -37,7 +37,7 @@ TEST(CharacterTest, UseItemTest) {
 }
 
 TEST(CharacterTest, DeathTest) {
-    Character testCharacter(100, 100, "TEST CHARACTER");
+    Character testCharacter(100, 100, 5, "TEST CHARACTER");
     testCharacter.setHealth(0);
     EXPECT_TRUE(testCharacter.isDead());
 }
